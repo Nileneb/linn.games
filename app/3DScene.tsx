0e5cdfdfa133b8f1d465a419c29e6d58a2d1ea24
Nileneb <144156631+Nileneb@users.noreply.github.com>
2024-11-20 06:31:07 +0100

    2

--- app/3DScene.tsx
+++ app/3DScene.tsx
@@ -1,18 +1,19 @@
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-export default function Scene() {
+const Scene = () => {
   return (
-    <div style={{ height: "100vh" }}>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color={"orange"} />
-        </mesh>
-      </Canvas>
-    </div>
+    <Canvas style={{ height: "400px", width: "100%" }}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} />
+      <mesh>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+      <OrbitControls />
+    </Canvas>
   );
-}
+};
+
+export default Scene;
